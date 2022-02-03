3909c0ff4c50da25de1bf048ca417f0634ec6598
LurkingVariable <32205318+LurkingVariable@users.noreply.github.com>
2022-02-02 20:57:07 -0600

    Update s.js

--- s.js
+++ s.js
@@ -1,3 +1,13 @@
+function arraysEqual(a, b) {
+    if (a.length != b.length)
+        return false;
+
+    for (var i=0; i < a.length; i++)
+        if (a[i] != b[i])
+            return false;
+    return true;
+}
+
 var ImageTransmitter = (function() {
     var btn;
     var fileinput;
